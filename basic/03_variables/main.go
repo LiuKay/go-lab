3a08e3798dc44909d2fbbb2dd93d05520c657aae
LiuKay <kayfen@foxmail.com>
2021-01-31 22:17:45 +0800

    update

---
+++ basic/03_variables/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int = 0
+	var s1 string = "this is b string"
+	str := "this is a string"
+	fmt.Println(num)
+	fmt.Println(s1)
+	fmt.Println(str)
+
+	//default value
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+	fmt.Println()
+}
